321302f30b2c55ed6e6756890c0808d4be1bba44
Vítor Galvão <info@vitorgalvao.com>
2016-12-02 02:16:10 +0000

    pokerstars.rb: support PT region

--- Casks/pokerstars.rb
+++ Casks/pokerstars.rb
@@ -2,17 +2,27 @@ cask 'pokerstars' do
   version :latest
   sha256 :no_check
 
-  url 'https://www.pokerstars.com/PokerStars.app.zip'
+  language 'us', default: true do
+    %w[com]
+  end
+
+  language 'pt' do
+    %w[pt PT]
+  end
+
+  # pokerstars was verified as official when first introduced to the cask
+  url "https://www.pokerstars.#{language[0]}/PokerStars#{language[1]}.app.zip"
   name 'PokerStars'
   homepage 'https://www.pokerstars.com/'
 
   auto_updates true
+  container nested: "PokerStars#{language[1]}/PokerStars#{language[1]}.dmg"
 
-  app 'PokerStars.app'
+  app "PokerStars#{language[1]}.app"
 
   zap delete: [
-                '~/Library/Preferences/com.pokerstars.user.ini',
-                '~/Library/Preferences/com.pokerstars.PokerStars.plist',
-                '~/Library/Application Support/PokerStars',
+                "~/Library/Preferences/com.pokerstars#{language[1]}.user.ini",
+                "~/Library/Preferences/com.pokerstars.PokerStars#{language[1]}.plist",
+                "~/Library/Application Support/PokerStars#{language[1]}",
               ]
 end
